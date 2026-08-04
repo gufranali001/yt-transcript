2a8f69c511d32079fb68437e40b1369d64291c7b
gufranali001 <gufranali8052@gmail.com>
2026-08-04 22:52:14 +0530

    Add sitemap and robots

---
+++ frontend/app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://yttubetranscript.com/sitemap.xml",
+  };
+}
