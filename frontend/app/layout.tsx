99053fe6affcf0952de595076e1c623aa0dc5b3e
gufranali001 <gufranali8052@gmail.com>
2026-08-04 23:57:05 +0530

    Add Google Analytics

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -94,6 +95,21 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         {children}
       </body>
+
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-7W1GVMV0Z1"
+        strategy="afterInteractive"
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-7W1GVMV0Z1');
+        `}
+      </Script>
     </html>
   );
 }
